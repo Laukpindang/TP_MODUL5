d7c2e393cbad82007999ec1c334ac2cb0fdfb64f
Laukpindang <ferdiansyahbandung3@gmail.com>
2026-03-28 09:57:26 +0700

    menambahkan class DataGeneric

--- TP_Modul5_103022400053/Program.cs
+++ TP_Modul5_103022400053/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+DataGeneric<string> dg = new DataGeneric<string>("103022400053");
+dg.PrintData();
+public class DataGeneric<T>
+{
+    private T data;
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + data);
+    }
+}
