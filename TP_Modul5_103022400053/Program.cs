10b48302e2ff99c3efa23673bbb35d0f1af64f05
Laukpindang <ferdiansyahbandung3@gmail.com>
2026-03-28 09:44:20 +0700

    menambahkan class HaloGeneric dengan method SapaUser

--- TP_Modul5_103022400053/Program.cs
+++ TP_Modul5_103022400053/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class HaloGeneric { 
+    public void SapaUser(string user)
+    {
+        Console.WriteLine("Halo user " + user);
+    }
+
+    public static void Main(string[] args) 
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser("Laukpindang");
+    }
+}
